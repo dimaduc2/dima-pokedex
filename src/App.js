d4b33f83e9e29380eb530fa584c185d2994fb878
Dima Duc <dimaduc@gmail.com>
2019-08-02 08:58:46 +0700

    logo va mau menu va ten menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+
+//Phần 1: các Import
+import React, { Component } from 'react'
+import { Menu, Image } from 'semantic-ui-react'
+
+import poke_logo from './Pokemon_symbol.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+
+//Phần 2: các State
+  state = {}
+
+//Phần 3: các Function
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div className="App">
+        <Menu borderless inverted style={{backgroundColor:'#8B0000'}}>
+          
+          <Menu.Item
+            name='pokemon'
+            active={activeItem === 'pokemon'}
+            onClick={this.handleItemClick}
+          >
+          <Image src={poke_logo} size='mini' />
+          <b>Pokedex</b>
+          </Menu.Item>
+
+          <Menu.Item
+            name='all'
+            active={activeItem === 'all'}
+            onClick={this.handleItemClick}
+          >
+          All
+          </Menu.Item>
+
+          <Menu.Item
+            name='type'
+            active={activeItem === 'type'}
+            onClick={this.handleItemClick}
+          >
+          Type
+          </Menu.Item>
+
+          <Menu.Item
+            name='compare'
+            active={activeItem === 'compare'}
+            onClick={this.handleItemClick}
+          >
+          Compare
+          </Menu.Item>
+
+          <Menu.Item
+            name='about'
+            active={activeItem === 'about'}
+            onClick={this.handleItemClick}
+          >
+          About
+          </Menu.Item>
+        </Menu>
+
+        <header className="App-header">
+          <img src={poke_logo} className="App-logo" alt="poke_logo" />
+        
+        </header>
+      </div>
+
+    )
+  }
+}
 export default App;
+
+
+
+
+
