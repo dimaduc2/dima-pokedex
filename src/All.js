9b45d26c52c7c2f63623eea198ee43e1933319c5
Dima Duc <dimaduc@gmail.com>
2019-08-18 08:00:50 +0700

    hop 3 anh pokemon

--- src/All.js
+++ src/All.js
@@ -3,10 +3,6 @@
 import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
 
-import Mega_Charizard_X from './Mega Charizard X Y Z/Mega Charizard X.png';
-import Mega_Charizard_Y from './Mega Charizard X Y Z/Mega Charizard Y.png';
-import Mega_Charizard_Z from './Mega Charizard X Y Z/Mega Charizard Z.png';
-
 //import './All.css';
 
 class All extends Component {
@@ -18,68 +14,36 @@ class All extends Component {
 
   render() {
     // const {  } = this.state
-    const { Eevees } = this.props;
+    const { Pokedex } = this.props;
     return (
       <div className="All" align="center">
-        <Image src={Mega_Charizard_X} size='medium' />
-        <br/>
-        <p>Mega Charizard X</p>
-        <p>Type: Fire vs Dragon</p>
-        <p>HP: 78</p>
-        <p>Attack: 130</p>
-        <p>Defense: 111</p>
-        <p>Sp. Atk: 130</p>
-        <p>Sp. Def: 85</p>
-        <p>Speed: 100</p>
-        <p>Total:	634</p>
+        
         <br/><br/>
-        <Image src={Mega_Charizard_Y} size='medium' />
-        <br/>
-        <p>Mega Charizard Y</p>
-        <p>Type: Fire vs Flying</p>
-        <p>HP: 78</p>
-        <p>Attack: 104</p>
-        <p>Defense: 78</p>
-        <p>Sp. Atk: 159</p>
-        <p>Sp. Def: 115</p>
-        <p>Speed: 100</p>
-        <p>Total:	634</p>
-        <br/><br/>
-        <Image src={Mega_Charizard_Z} size='medium' />
-        <br/>
-        <p>Mega Charizard Z (X + Y)</p>
-        <p>Mega Charizard X</p>
-        <p>Type: Fire vs Flying vs Dragon</p>
-        <p>HP: 78</p>
-        <p>Attack: 130</p>
-        <p>Defense: 111</p>
-        <p>Sp. Atk: 159</p>
-        <p>Sp. Def: 115</p>
-        <p>Speed: 100</p>
-        <p>Total:	693</p>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-
+        
         {
-          Object.keys(Eevees).map(
+          Object.keys(Pokedex).map(
             (moiSo) => 
             <div>
-              <Image src={Eevees[moiSo].picture} size='medium' />
-              <h1>{Eevees[moiSo].name}</h1>
-              <p>{Eevees[moiSo].types}</p>  
-              <p>{Eevees[moiSo].hp}</p>
-              <p>{Eevees[moiSo].speed}</p>
+              <Image src={Pokedex[moiSo].picture} size='medium' />
+              <h1>{Pokedex[moiSo].name}</h1>
+              <p>{Pokedex[moiSo].types}</p>  
+              <p>HP {Pokedex[moiSo].hp}</p>
+              <p>Attack {Pokedex[moiSo].attack}</p>
+              <p>Defense {Pokedex[moiSo].defense}</p>
+              <p>Sp. Atk {Pokedex[moiSo].sp_atk}</p>
+              <p>Sp. Def {Pokedex[moiSo].sp_def}</p>
+              <p>Speed {Pokedex[moiSo].speed}</p>
+              <p>Total {Pokedex[moiSo].hp + Pokedex[moiSo].attack + Pokedex[moiSo].defense + 
+                        Pokedex[moiSo].sp_atk + Pokedex[moiSo].sp_def + Pokedex[moiSo].speed}</p>
               <br/><br/>
             </div>
           )
         }
         
+        
+
       </div>
     )
   }
 }
 export default All;
-
-
-
-
-
--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ import Type from './Type';
 import PokeBall from './PokeBall';
 import About from './About';
 
+import Genesect_Normal from './Genesect/Genesect (Normal).png';
+import Genesect_Electric from './Genesect/Genesect (Electric) - Shock Drive.png';
+import Genesect_Fire from './Genesect/Genesect (Fire) - Burn Drive.png';
+import Genesect_Ice from './Genesect/Genesect (Ice) - Chill Drive.png';
+import Genesect_Water from './Genesect/Genesect (Water) - Douse Drive.png';
+
+import Mega_Charizard_X from './Mega Charizard X Y Z/Mega Charizard X.png';
+import Mega_Charizard_Y from './Mega Charizard X Y Z/Mega Charizard Y.png';
+import Mega_Charizard_Z from './Mega Charizard X Y Z/Mega Charizard Z.png';
+
 import Eevee from './Eeveelutions/133 Eevee (Normal).jpg';
 import Vaporeon from './Eeveelutions/134 Vaporeon (Water).jpg';
 import Jolteon from './Eeveelutions/135 Jolteon (Electric).jpg';
@@ -23,69 +33,194 @@ import Leafeon from './Eeveelutions/470 Leafeon (Grass).jpg';
 import Glaceon from './Eeveelutions/471 Glaceon (Ice).jpg';
 import Sylveon from './Eeveelutions/700 Sylveon (Fairy).jpg';
 
-const Eeveelutions = {
+const Pokemon = {
+  'GenesectN':{
+    name: 'Genesect Normal',
+    picture: Genesect_Normal,
+    types: ['Bug Steel (Normal)'],
+    hp: 71,
+    attack: 120,
+    defense: 95,
+    sp_atk: 120,
+    sp_def: 95,
+    speed: 99
+  },
+  'GenesectE':{
+    name: 'Genesect Electric',
+    picture: Genesect_Electric,
+    types: ['Bug Steel (Electric)'],
+    hp: 71,
+    attack: 120,
+    defense: 95,
+    sp_atk: 120,
+    sp_def: 95,
+    speed: 99
+  },
+  'GenesectF':{
+    name: 'Genesect Fire',
+    picture: Genesect_Fire,
+    types: ['Bug Steel (Fire)'],
+    hp: 71,
+    attack: 120,
+    defense: 95,
+    sp_atk: 120,
+    sp_def: 95,
+    speed: 99
+  },
+  'GenesectI':{
+    name: 'Genesect Ice',
+    picture: Genesect_Ice,
+    types: ['Bug Steel (Ice)'],
+    hp: 71,
+    attack: 120,
+    defense: 95,
+    sp_atk: 120,
+    sp_def: 95,
+    speed: 99
+  },
+  'GenesectW':{
+    name: 'Genesect Water',
+    picture: Genesect_Water,
+    types: ['Bug vs Steel (Water)'],
+    hp: 71,
+    attack: 120,
+    defense: 95,
+    sp_atk: 120,
+    sp_def: 95,
+    speed: 99
+  },
+  'CharizardX':{
+    name: 'Mega Charizard X',
+    picture: Mega_Charizard_X,
+    types: ['Fire vs Dragon'],
+    hp: 78,
+    attack: 130,
+    defense: 111,
+    sp_atk: 130,
+    sp_def: 85,
+    speed: 100
+  },
+  'CharizardY':{  
+    name: 'Mega Charizard Y',
+    picture: Mega_Charizard_Y,
+    types: 'Type: Fire vs Flying',
+    hp: 78,
+    attack: 104,
+    defense: 78,
+    sp_atk: 159,
+    sp_def: 115,
+    speed: 100
+  },
+  'CharizardZ':{  
+    name: 'Mega Charizard Z (X + Y)',
+    picture: Mega_Charizard_Z,
+    types: ['Fire vs Flying vs Dragon'],
+    hp: 78,
+    attack: 130,
+    defense: 111,
+    sp_atk: 159,
+    sp_def: 115,
+    speed: 100
+  },
   '133': {
     name: 'Eevee',
     picture: Eevee,
     types: ['Normal'],
     hp: 55,
-    speed: 55,
+    attack: 55,
+    defense: 50,
+    sp_atk: 45,
+    sp_def: 65,
+    speed: 55
   },
   '134': {
     name: 'Vaporeon',
     picture: Vaporeon,
     types: ['Water'],
     hp: 130,
-    speed: 65,
+    attack: 65,
+    defense: 60,
+    sp_atk: 110,
+    sp_def: 95,
+    speed: 65
   },
   '135': {
     name: 'Jolteon',
     picture: Jolteon,
     types: ['Electric'],
     hp: 65,
-    speed: 130,
+    attack: 65,
+    defense: 60,
+    sp_atk: 110,
+    sp_def: 95,
+    speed: 130
+    
   },
   '136': {
     name: 'Flareon',
     picture: Flareon,
     types: ['Fire'],
     hp: 65,
-    speed: 65,
+    attack: 130,
+    defense: 60,
+    sp_atk: 95,
+    sp_def: 110,
+    speed: 65
   },
   '196': {
     name: 'Espeon',
     picture: Espeon,
     types: ['Psychic'],
     hp: 65,
-    speed: 110,
+    attack: 65,
+    defense: 60,
+    sp_atk: 130,
+    sp_def: 95,
+    speed: 110
   },
   '197': {
     name: 'Umbreon',
     picture: Umbreon,
     types: ['Dark'],
     hp: 95,
-    speed: 65,
+    attack: 65,
+    defense: 110,
+    sp_atk: 60,
+    sp_def: 130,
+    speed: 65
   },
   '470': {
     name: 'Leafeon',
     picture: Leafeon,
     types: ['Grass'],
     hp: 65,
-    speed: 95,
+    attack: 110,
+    defense: 130,
+    sp_atk: 60,
+    sp_def: 65,
+    speed: 95
   },
   '471': {
     name: 'Glaceon',
     picture: Glaceon,
     types: ['Ice'],
     hp: 65,
-    speed: 65,
+    attack: 60,
+    defense: 110,
+    sp_atk: 130,
+    sp_def: 95,
+    speed: 65
   },
   '700': {
     name: 'Sylveon',
     picture: Sylveon,
     types: ['Fairy'],
     hp: 95,
-    speed: 60,
+    attack: 65,
+    defense: 65,
+    sp_atk: 110,
+    sp_def: 130,
+    speed: 60
   }
 }
 
@@ -166,8 +301,7 @@ class App extends Component {
         </Menu>
 
         {activeItem === "all"
-          ? <All Eevees = {Eeveelutions} ></All>
-          // ? <p>sdfsf</p>  
+          ? <All Pokedex = {Pokemon} ></All>
           : null
         }
 
